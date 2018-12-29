25510f9fe9c87ee53d96ce0928457ec050b73210
Michael <michael.walker@chifleytech.com>
2018-12-29 11:37:36 +1100

    model

---
+++ model.sql
@@ -0,0 +1,14 @@
+CREATE TABLE joined AS
+select
+	g_tbl.id as g_keys,
+	a_tbl.id as a_keys
+from
+	google_tokens g_tbl
+inner join
+	amazon_tokens a_tbl
+on
+	g_tbl.blocking_key=a_tbl.blocking_key;
+
+create table actual  as select keys from frequency where freq < 3;
+
+
